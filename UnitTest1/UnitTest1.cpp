6a48c0504688afec63f9c5d8d5d67de3eccea494
roma protsak <roman.protsak.it.2021@lpnu.ua>
2022-06-14 12:31:17 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../6.9/6.9.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Node* root = NULL;
+			int i = Count(root);
+			Assert::AreEqual(i, 0);
+		}
+	};
+}
